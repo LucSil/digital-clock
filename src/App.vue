ebb529da63c0e267b59d40fbbdcf094093e82521
LucSil <luciansilva351@gmail.com>
2022-02-24 13:34:22 +0100

    completed functionality and added styling

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,20 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  box-sizing: border-box;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
 }
 </style>
--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,28 +1,119 @@
 <template>
-  <div class="clock">
-    <span class="hr">{{ hr }}</span> : <span class="min">{{ min }}</span> :
-    <span class="sec">{{ sec }}</span> <span class="ampm">{{ ampm }}</span>
+  <div class="digital">
+    <div class="day">
+      <span class="dayName">{{ dayName }}</span
+      >,
+      <span class="month">{{ month }}</span>
+      <span clas="dayNum">{{ dayNum }}</span
+      >,
+      <span class="year">{{ year }}</span>
+    </div>
+    <div class="time">
+      <span class="hr">{{ hr }}</span
+      >:<span class="min">{{ min }}</span
+      >:<span class="sec">{{ sec }}</span
+      ><span :class="[ampm == 'AM' ? 'am' : 'pm']">{{ ampm }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
 export default {
-  setup() {
-    let d = new Date();
+  data() {
+    return {
+      hr: "",
+      min: "",
+      sec: "",
+      ampm: "",
+      dayName: "",
+      dayNum: "",
+      month: "",
+      year: "",
 
-    let hr = setInterval(() => {
-      d.getHours();
-    }, 1000);
-    let min = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
-    let sec = setInterval(() => {
-      d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds();
-    }, 1000);
-    let ampm = d.getHours() >= 12 ? "PM" : "AM";
+      week: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
+      months: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    };
+  },
 
-    return { hr, min, sec, ampm };
+  methods: {
+    updateTime: function () {
+      let d = new Date();
+
+      //day
+      this.dayName = this.week[d.getDay()];
+      this.dayNum = d.getDate();
+      this.month = this.months[d.getMonth()];
+      this.year = d.getFullYear();
+
+      //time
+      this.hr = d.getHours();
+      this.min = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
+      this.sec = d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds();
+      this.ampm = this.hr > 12 ? "pm" : "am";
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
   },
 };
 </script>
 
-<style></style>
+<style>
+.digital {
+  font-family: Cursive;
+  font-size: 1.5rem;
+  display: grid;
+  place-items: center;
+  grid-template-rows: repeat(2, 1fr);
+  box-shadow: 2px 3px 10px 0px rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+}
+.day {
+  color: rgb(211, 208, 208);
+  padding: 10px 20px;
+  background: rgb(1, 30, 37);
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+
+.dayName {
+  color: white;
+  font-size: 2.5rem;
+  font-weight: 600;
+}
+
+.month {
+  margin: 0 10px 0 5px;
+}
+.year {
+  margin-left: 5px;
+}
+.time {
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+  font-size: 2rem;
+}
+.hr {
+  font-weight: 800;
+  margin-right: 5px;
+}
+.min,
+.sec {
+  margin: 0 5px;
+}
+</style>
